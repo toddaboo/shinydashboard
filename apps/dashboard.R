4e9d02f6aa59ec981cde629c101049b87d8a8c33
toddaboo <todd.grabowsky@gmail.com>
2016-11-04 17:03:35 -0400

    Update dashboard.R

--- apps/dashboard.R
+++ apps/dashboard.R
@@ -18,33 +18,10 @@ fluidRow(
 #        )
 #     )  
 #   )
-  box(collapsible=TRUE, title="Some More Evidence..."
-      ,tags$div(class="panel-body", id = "sort3"
-         ,tags$li("item one")
-         ,tags$li("item two")
-         ,tags$li("item three")
-         ,tags$li("item four")
-      )
+  box(collapsible=TRUE, title="Title Goes Here"
+      "placeholder"
   )
-  ,box(collapsible=TRUE, title="Some Evidence..."
-    ,tags$div(class="panel-body", id = "sort1"
-        ,lapply(
-          colnames(mtcars)
-          ,function(co){
-            tag("p"
-              ,list(
-                class = class(mtcars[,co])
-                ,tags$span( class = "glyphicon glyphicon-move" )
-                ,tags$strong( co  )
-              )
-            )
-          }
-        )      
-      )
-    )       
-  # make specified items draggable for evidence
-  ,sortableR( "sort1", options = list( group = list(name="evidenceGroup", pull = "clone", put = FALSE) ) )
-  ,sortableR( "sort3", options = list( group = list(name="evidenceGroup", pull = "clone", put = FALSE) ) )
-  ,sortableR( "dPlot", options = list( group = list(name="evidenceGroup", pull = "clone", put = FALSE) ) )
-#   ,sortableR( "sort2", options = list( group = "sortGroup1" ) )
+  ,box(collapsible=TRUE, title="Title Goes Here"
+      "placeholder"  
+  )       
 )
